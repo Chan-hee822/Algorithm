a441906aa379a8e7291bba4c45025911af4b26f2
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-01 12:44:44 +0900

    [level 2] Title: 주차 요금 계산, Time: 3.09 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -0,0 +1,82 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    int baseTime;
+    int baseFee;
+    int perTime;
+    int perFee;
+
+    public int[] solution(int[] fees, String[] records) {
+
+        Map<String, Integer> totalTime = new HashMap<>();
+        Map<String, Integer> carInTime = new HashMap<>();
+        Map<String, Integer> carTotalFee = new HashMap<>();
+
+        baseTime = fees[0];
+        baseFee = fees[1];
+        perTime = fees[2];
+        perFee = fees[3];
+
+        for (String record : records) {
+            String[] recordArr = record.split(" ");
+            int time = getMin(recordArr[0]);
+            String carNumber = recordArr[1];
+            String carState = recordArr[2];
+            //
+            if (!carInTime.containsKey(carNumber)) {
+                carInTime.put(carNumber, time);
+            } else {
+                int inTime = carInTime.get(carNumber);
+                int outTime = time;
+//                int finalFee =getFee(outTime, inTime);
+                if (totalTime.containsKey(carNumber)) {
+                    totalTime.put(carNumber, totalTime.get(carNumber) + (outTime - inTime));
+                } else {
+                    totalTime.put(carNumber, outTime - inTime);
+                }
+                carInTime.remove(carNumber);
+            }
+        }
+        if (!carInTime.isEmpty()) {
+            for (Map.Entry<String, Integer> item : carInTime.entrySet()) {
+                String carNumber = item.getKey();
+                int inTime = item.getValue();
+                int outTime = getMin("23:59");
+                if (totalTime.containsKey(carNumber)) {
+                    totalTime.put(carNumber, totalTime.get(carNumber) + (outTime - inTime));
+                } else {
+                    totalTime.put(carNumber, outTime - inTime);
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> item : totalTime.entrySet()) {
+            String carNumber = item.getKey();
+            int fee = getFee(item.getValue());
+            carTotalFee.put(carNumber, fee);
+        }
+        int[] answer = carTotalFee.entrySet()
+                .stream()
+                .sorted((a, b) -> Integer.parseInt(a.getKey()) - Integer.parseInt(b.getKey()))
+                .mapToInt(Map.Entry::getValue)
+                .toArray();
+        return answer;
+    }
+
+    public int getFee(int totalTime) {
+        if (totalTime <= baseTime) {
+            return baseFee;
+        }
+        totalTime -= baseTime;
+        totalTime = totalTime % perTime == 0 ? totalTime / perTime : totalTime / perTime + 1;
+        return baseFee + totalTime * perFee;
+    }
+
+    public static int getMin(String time) {
+        String[] timeArr = time.split(":");
+        int hour = Integer.parseInt(timeArr[0]);
+        int min = Integer.parseInt(timeArr[1]);
+        return hour * 60 + min;
+    }
+}
