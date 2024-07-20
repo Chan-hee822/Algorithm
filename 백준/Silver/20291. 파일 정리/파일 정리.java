30f84724217583ae9c20d66a09d2ec678a821638
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-20 11:05:04 +0900

    [Silver III] Title: 파일 정리, Time: 1428 ms, Memory: 81456 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String file = st.nextToken().split("\\.")[1];
+            map.put(file, map.getOrDefault(file, 0) + 1);
+        }
+
+        int size = map.size();
+        List<String> list = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> item : map.entrySet()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(item.getKey()).append(" ").append(item.getValue());
+            list.add(sb.toString());
+        }
+
+        list = list.stream().sorted().collect(Collectors.toList());
+        for (String f : list) {
+            System.out.println(f);
+        }
+    }
+}
