e0b5875898586cb75fee3b32a639cf94a37dc652
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-22 18:01:15 +0900

    [Silver IV] Title: 좋은 단어, Time: 548 ms, Memory: 56136 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            Stack<String> stack = new Stack<>();
+            String[] word = br.readLine().split("");
+            for (String item : word) {
+
+                if (stack.isEmpty()) {
+                    stack.push(item);
+                    continue;
+                }
+
+                if (stack.peek().equals(item)) {
+                    stack.pop();
+                } else {
+                    stack.push(item);
+                }
+                continue;
+            }
+            if (stack.isEmpty()){
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
