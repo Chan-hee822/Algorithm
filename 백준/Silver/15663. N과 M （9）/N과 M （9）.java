6130b4efb5627e760914ff797af884084ff77073
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-22 22:38:59 +0900

    [Silver II] Title: N과 M (9), Time: 228 ms, Memory: 21672 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,30 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static boolean[] visited;
     static int[] result;
     static int[] numArr;
-    static Set<String> set;
 
     public static void main(String[] args) throws IOException {
-
-        /**
-         *  dfs 순열 만들기
-         *  사용한 숫자 visited 배열로 체크
-         *  사용할 숫자 true, 사용 끝나면 false
-         *  TreeSet을 사용해서 순열 출력
-         *
-         */
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 재료 개수
         N = Integer.parseInt(st.nextToken());
-        // 깊이
         M = Integer.parseInt(st.nextToken());
 
         visited = new boolean[N];
@@ -33,31 +22,28 @@ public class Main {
         numArr = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt).sorted().toArray();
 
-        set = new HashSet<>();
-        StringBuilder answer = new StringBuilder();
-        dfs(0, answer);
-        System.out.println(answer.toString());
+        StringBuilder sb = new StringBuilder();
+        dfs(0, sb);
+        System.out.println(sb.toString());
     }
 
-    private static void dfs(int depth, StringBuilder answer) {
+    private static void dfs(int depth, StringBuilder sb) {
         if (depth == M) {
-            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < M; i++) {
                 sb.append(result[i]).append(" ");
             }
-            if (!set.contains(sb.toString())) {
-                set.add(sb.toString());
-                answer.append(sb).append('\n');
-            }
+            sb.append('\n');
             return;
         }
 
+        int lastUsed = -1;
         for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
+            if (!visited[i] && numArr[i] != lastUsed) {
                 visited[i] = true;
                 result[depth] = numArr[i];
-                dfs(depth + 1, answer);
+                dfs(depth + 1, sb);
                 visited[i] = false;
+                lastUsed = numArr[i];
             }
         }
     }
