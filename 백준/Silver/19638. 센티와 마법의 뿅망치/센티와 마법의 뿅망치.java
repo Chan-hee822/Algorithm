9a5865895e168ca082999d42cf1c73ee8b9766ee
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-25 14:28:12 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 416 ms, Memory: 32536 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -6,6 +6,16 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        /**
+         * 우선순위 큐 사용해여 풀이
+         * 거인들의 키 내림차순으로 큐에 담음
+         * 큐에 피크값(거인들 중 가장 큰 키) 센티를 비교, 크면 하나씩 hammer수 줄여가며 체크
+         * hammer 수가 0이 되거나 피크 값이 센티보다 작을 때 까지 반복
+         * 피크 값이 크다면 큐에서 pop --> 해당 값 / 2 -> 큐에 push -> hammer--
+         * 만약 피크 값이 1이면 hammer 효과 없기 때문 반복 종료
+         * 주의) 처음부터 센티보다 작은 거인도 존재
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -22,15 +32,11 @@ public class Main {
         int cnt = hammer;
         while (hammer > 0) {
 
-            if (height.peek() < centi) {
+            if (height.peek() < centi || height.peek() == 1) {
                 break;
             }
 
             int temp = height.poll();
-            if (temp == 1) {
-                height.add(temp);
-                break;
-            }
             hammer--;
             height.add(temp / 2);
         }
