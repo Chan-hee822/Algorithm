e4b7cf81ff883cd891bcae82ff5928efa7d72c9f
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-29 18:06:33 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1248 ms, Memory: 272088 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] lists = new List[N + 1];
+        for (int i = 0; i <= N ; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            lists[start].add(end);
+        }
+        List<Integer> result = new ArrayList<>();
+
+        bfs(lists, K, X, result);
+
+        if (result.isEmpty()) {
+            System.out.println(-1);
+        }
+        List<Integer> sortedList = result.stream().sorted().collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder();
+        for (Integer num : sortedList) {
+            sb.append(num).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static void bfs(List<Integer>[] lists, int K, int X, List<Integer> result) {
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{X, 0});
+        int[] distances = new int[lists.length];
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int start = cur[0];
+            int cnt = cur[1];
+
+            if (start == X && cnt > 0) {
+                continue;
+            }
+
+            if (cnt == K) {
+                result.add(start);
+            }
+
+            for (int i = 0; i < lists[start].size(); i++) {
+                int newStart = lists[start].get(i);
+                if (distances[newStart] == 0) {
+                    distances[newStart] = distances[start] + 1;
+                    queue.add(new int[]{newStart, cnt + 1});
+                }
+            }
+        }
+    }
+}
