7ed01a78d49d8c5cddf1de483d6deb28d870e779
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-31 14:30:01 +0900

    [Silver V] Title: 두라무리 휴지, Time: 416 ms, Memory: 32492 KB -BaekjoonHub

--- 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.java
+++ 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -12,29 +12,30 @@ public class Main{
         String first = br.readLine();
         String second = br.readLine();
 
-        String sortedFirst = Arrays.stream(first.split("")).sorted().collect(Collectors.joining());
-        String sortedSecond = Arrays.stream(second.split("")).sorted().collect(Collectors.joining());
-
-        if (!sortedFirst.equals(sortedSecond)) {
+        // 두 문자열이 동일한지, 시작과 끝이 같은지 확인
+        if (first.charAt(0) != second.charAt(0) || first.charAt(N - 1) != second.charAt(N - 1)) {
             System.out.println("NO");
             return;
         }
 
-        if (first.charAt(0) != second.charAt(0) || first.charAt(N - 1) != second.charAt(N - 1)
-        ) {
-            System.out.println("NO");
-            return;
-        }
+        // 모음을 제거한 문자열 비교
         String firstWithoutVowel = first.replaceAll("[aeiou]", "");
         String secondWithoutVowel = second.replaceAll("[aeiou]", "");
-
         if (!firstWithoutVowel.equals(secondWithoutVowel)) {
             System.out.println("NO");
             return;
         }
 
+        // 남은 모음들을 정렬하여 비교
+        char[] firstVowels = first.replaceAll("[^aeiou]", "").toCharArray();
+        char[] secondVowels = second.replaceAll("[^aeiou]", "").toCharArray();
+        Arrays.sort(firstVowels);
+        Arrays.sort(secondVowels);
+        if (!Arrays.equals(firstVowels, secondVowels)) {
+            System.out.println("NO");
+            return;
+        }
+
         System.out.println("YES");
-
-
     }
 }
