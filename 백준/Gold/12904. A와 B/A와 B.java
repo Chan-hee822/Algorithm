fa5a8d6196e68485da73b3fe3c8f3daa1c7895db
Chan-hee822 <124652331+Chan-hee822@users.noreply.github.com>
2024-07-31 22:39:27 +0900

    [Gold V] Title: A와 B, Time: 136 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,24 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String before = br.readLine();
         String after = br.readLine();
 
-        while (before.length() != after.length()) {
-            int size = after.length();
-            if (after.charAt(size - 1) == 'A') {
-                after = after.substring(0, size - 1);
+        Deque<String> dq = new LinkedList<>();
+        Collections.addAll(dq, after.split(""));
+        boolean flag = false;
+        while (before.length() != dq.size()) {
+            if (!flag) {
+                String poll = dq.pollLast();
+                if (poll.equals("A")) {
+                    continue;
+                }
+                flag = true;
                 continue;
             }
-            after = after.substring(0, size -1);
-            StringBuilder sb = new StringBuilder();
-            after = sb.append(after).reverse().toString();
+            String poll = dq.poll();
+            if (poll.equals("A")) {
+                continue;
+            }
+            flag = false;
         }
-
+        StringBuilder sb = new StringBuilder();
+        while (!dq.isEmpty()) {
+            sb.append(dq.poll());
+        }
+        after = flag ? sb.reverse().toString()
+                : sb.toString();
         System.out.println(before.equals(after) ? 1 : 0);
 
     }
